c083da931400fb6593344cce0ebaec7ff24cc37f
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-04-28 16:03:29 -0400

    Update Index.js

--- Church/src/Index.js
+++ Church/src/Index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
     document.querySelector('#root')
 );
+
