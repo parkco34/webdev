548239263c64b6cab1ddd8e6c593770c427fc5a1
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-03 21:12:36 -0400

    Update NavBar.js

--- Church/src/components/NavBar.js
+++ Church/src/components/NavBar.js
@@ -3,14 +3,52 @@ import styles from '../cssModules/NavBar.module.css';
 import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.listener = null;
+        this.state = {
+            status: "up"
+        };
+    }
+
+    componentDidMount() {
+        this.listener = document.addEventListener("scroll", e => {
+            const scrolled = document.scrollingElement.scrollTop;
+
+            if (scrolled >= 120) {
+                if (this.state.status !== "down") {
+                    this.setState({status: "down"});
+                }
+            }
+
+            else {
+                if (this.state.status !== "up") {
+                    this.setState({status: "up"});
+                }
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        document.removeEventListener("scroll", this.listener);
+    }
+
     render() {
         return (
-            <div className={styles.spacer}>
+            <div
+                className={styles.spacer}
+                style={{backgroundColor:
+                        this.state.status === "up" ?
+                        "transparent" : "white",
+                        position: "fixed",
+                        transition: "all 0.5s"
+            }}>
                 <div className={styles.navbar}>
-                    <p className={styles.signature}>UCC</p>
-                    <Link to="/" style={{ textDecoration: 'none' }}><span className={styles.navbtn}>Btn 1</span></Link>
-                    <Link to="/about" style={{textDecoration: 'none'}}><span className={styles.navbtn}>Btn 2</span></Link>
-                    <Link to="/join" style={{textDecoration: 'none'}}><span className={styles.navbtn}>Btn 3</span></Link>
+                    <p className={styles.signature} style={{fontSize: "155%"}}>UCC</p>
+                    <Link to="/"><span className={styles.navbtn}>Home</span></Link>
+                    <Link to="/about"><span className={styles.navbtn}>About</span></Link>
+                    <Link to="/join"><span className={styles.navbtn}>Join</span></Link>
                 </div>
             </div>
         );
@@ -18,4 +56,3 @@ class NavBar extends React.Component {
 }
 
 export default NavBar;
-
