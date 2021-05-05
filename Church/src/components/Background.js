68331050795c09b799f8675e87df94389d11f9f1
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-04 21:26:43 -0400

    Update Background.js

--- Church/src/components/Background.js
+++ Church/src/components/Background.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '../cssModules/Background.module.css';
 import mainImage from '../Images/cross_fractal.webp';
 import mobileImage from '../Images/devil.webp';
 import styles from '../cssModules/Background.module.css';
 import NavBar from './NavBar';
 
-/* The empty array as a second argument prevents the useEffect hook from running every time the component updates (only when it mounts and unmounts is it run). <---????    */
-useEffect(() => {
-    window.addEventListener('resize', handleWindowResize);
-
-    return () => {
-        window.removeEventListener('resize', handleWindowResize);
-    }
-}, []);
 
 const Background = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    /* The empty array as a second argument prevents the useEffect hook from running every time the component updates (only when it mounts and unmounts is it run). <---????    */
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
+
     const image = window.innerWidth >= 650 ? mainImage : mobileImage;
 
     return (
