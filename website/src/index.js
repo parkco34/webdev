0bb5ec7b847c225a54cb6dccaee3c64be64cd805
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-17 15:50:22 -0400

    Delete website/src directory

--- website/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './components/style.css';
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
