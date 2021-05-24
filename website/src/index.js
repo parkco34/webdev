8049c0803d7f92bf165defacb7094c520558431c
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-24 18:23:19 -0400

    Create index.js

---
+++ website/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './components/App';
+import App from './comps/App';
+//import './components/style.css';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
