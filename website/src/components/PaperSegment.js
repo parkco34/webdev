0d26dc3acd789c9f7a2d4ac9d52544b78887de82
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-24 15:45:58 -0400

    Update PaperSegment.js

--- website/src/components/PaperSegment.js
+++ website/src/components/PaperSegment.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import img1 from '../myImages/robot2.webp';
-import img2 from'../myImages/robot1.webp';
-import img3 from '../myImages/robot3.webp';
+import img1 from '../Images/robot2.webp';
+import img2 from'../Images/robot1.webp';
+import img3 from '../Images/robot3.webp';
 import ImageList from './ImageList.js';
 
 const useStyles = makeStyles(theme => ({
@@ -41,6 +41,7 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
+// Refactor this shit, please...
 const PaperSegment = () => {
 	const classes = useStyles();
 
@@ -59,16 +60,21 @@ const PaperSegment = () => {
 		shadow: 1,
 	});
 
+	const [clicked, setClicked] = useState(false);
+	
+	const handleOnClick = () => setClicked(!clicked);
+
 	return (
         <div>
             <section className={classes.toproot}>
                 <div >
                     <Paper
-                        className={classes.root}
                         classes={{root: expand1.raised ? classes.paperHovered : ""}}
+						onClick={handleOnClick}
                         onMouseOver={()=>setExpand1({raised: true, shadow: 3})}
                         onMouseOut={()=>setExpand1({raised: false, shadow: 1})}
                         raised={expand1.raised ? 1 : 0} zdepth={expand1.shadow}
+                        className={classes.root}
                         style={{backgroundImage: `url(${img1})`}}
                         elevation={3}
                     >
@@ -80,12 +86,12 @@ const PaperSegment = () => {
 
                 <div>
                     <Paper
-                        style={{backgroundImage: `url(${img2})`}}
                         classes={{root: expand2.raised ? classes.paperHovered : ""}}
                         onMouseOver={()=>setExpand2({raised: true, shadow: 3})}
                         onMouseOut={()=>setExpand2({raised: false, shadow: 1})}
                         raised={expand2.raised ? 1 : 0} zdepth={expand2.shadow}
                         className={classes.root}
+                        style={{backgroundImage: `url(${img2})`}}
                         elevation={3}
                     >
                         <div>
@@ -96,12 +102,12 @@ const PaperSegment = () => {
 
                 <div>
                     <Paper
-                        style={{backgroundImage: `url(${img3})`}}
                         classes={{root: expand3.raised ? classes.paperHovered : ""}}
                         onMouseOver={()=>setExpand3({raised: true, shadow: 3})}
                         onMouseOut={()=>setExpand3({raised: false, shadow: 1})}
                         raised={expand3.raised ? 1 : 0} zdepth={expand3.shadow}
                         className={classes.root}
+                        style={{backgroundImage: `url(${img3})`}}
                         elevation={3}
                     >
                         <div>
@@ -110,12 +116,13 @@ const PaperSegment = () => {
                     </Paper>
                 </div>
             </section>
-            <div className="da-images">
-                <ImageList />
-            </div>
+			{clicked && 
+				<section className={classes.toproot}>
+					<ImageList />
+				</section>
+			}
         </div>
-	)
+	);
 }
 
 export default PaperSegment;
-
