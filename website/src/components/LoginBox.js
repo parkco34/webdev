2e6aa419a6df7317ec45f5c0d4596ad3c0c11953
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-26 15:50:20 -0400

    Update LoginBox.js

--- website/src/components/LoginBox.js
+++ website/src/components/LoginBox.js
@@ -1,58 +1,88 @@
-import React, { Component } from "react";
-import '../cssModules/LoginBox.module.css';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-export default function LoginBox() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [country, setCountry] = React.useState("");
-  const [acceptedTerms, setAcceptedTerms] = React.useState(false);
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: theme.spacing(2),
+		
+		'& .MuiTextField-root': {
+			margin: theme.spacing(1),
+			width: '300px',
+		},
+		'& .MuiButtonBase-root': {
+			margin: theme.spacing(2),
+		},
+	},
+	link: {
+		curso: 'pointer',
+	},
+}));
 
-  const handleSubmit = (event) => {
-    console.log(`
-      Email: ${email}
-      Password: ${password}
-      Country: ${country}
-      Accepted Terms: ${acceptedTerms}
-    `);
-    
-    event.preventDefault();
-  }
+const LoginBox = ({ handleClose }) => {
+	const classes = useStyles();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Create Account</h1>
+	// declare a state variable for each input
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-      <label>
-        Email:
-        <input
-          name="email"
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required />
-      </label>
-      
-      <label>
-        Password:
-        <input
-          name="password"
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required />
-      </label>
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log(firstName, lastName, email, password);
 
-      <label>
-        <input
-          name="acceptedTerms"
-          type="checkbox"
-          onChange={e => setAcceptedTerms(e.target.value)}
-          required />
-        I accept the terms of service        
-      </label>
+		handleClose();
+	};
 
-      <button>Submit</button>
-    </form>
-  );
+	return (
+		<form className={classes.root}>
+			<TextField 
+				label="First Name" 
+				variant="filled" 
+				value={firstName}
+				required 
+				onChange={e=>setFirstName(e.target.value)}
+			/>
+
+			<TextField 
+				label="Last Name" 
+				variant="filled" 
+				value={lastName}
+				required 
+				onChange={e=>setLastName(e.target.value)}
+			/>
+
+			<TextField 
+				label="Email"	
+				variant="filled" 
+				value={email}
+				required 
+				onChange={e=>setEmail(e.target.value)}
+			/>
+
+			<TextField 
+				label="Password" 
+				type="password" 
+				variant="filled"
+				value={password}
+				required 
+				onChange={e=>setPassword(e.target.value)}
+			/>
+
+			<div>
+				<Button variant="contained" onClick={handleClose}>Cancel</Button>
+				<Button type="submit" variant="contained" style={{backgroundColor: '#2d6bcf', color: 'white'}} >
+					Signup
+				</Button>
+			</div>
+		</form>
+	);
 }
 
+export default LoginBox;
