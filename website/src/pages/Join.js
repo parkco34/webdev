935be17ae0b48ee0914242f9b342a1bd5a1f7c39
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-26 15:53:57 -0400

    Update Join.js

--- website/src/pages/Join.js
+++ website/src/pages/Join.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Login from '../components/Login';
+
+const useStyles = makeStyles({
+	root: {
+		background:
+	}
+})
 
 class Join extends Component {
 	render() {
