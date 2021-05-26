e303bb12c03e379ae29ec3a9559ccd28904bb7da
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-26 15:50:05 -0400

    Update Login.js

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -1,24 +1,37 @@
 // Passing props to state and syncing props to state using Hooks:
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 import LoginBox from '../components/LoginBox';
+import ModalDialog from './ModalDialog';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			isLoginOpen: true,
-			isRegisterOpen: false,
+			isDialogOpen: false,
 		};
+
+		this.handleOpen = this.handleOpen.bind(this);
+	}
+
+	handleOpen() {
+		this.setState(state => ({
+			isDialogOpen: !state.isDialogOpen
+		}));
 	}
 
 	render() {
 		return (
-			<div className="root-container">
-				<LoginBox />
+			<div className="Login">
+				<Button variant="contained" color="primary" onClick={this.handleOpen}>
+					Signup
+				</Button>
+				
+				<ModalDialog open={this.state.isDialogOpen} handleClose={this.handleOpen}/>
 			</div>
 		);
 	}
-}
+};
 
 export default Login;
