59e33919206f85612dff2f89023683d4e90ed831
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-30 17:26:33 -0400

    Update NavBar.js

--- website/src/components/NavBar.js
+++ website/src/components/NavBar.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import styles1 from '../cssModules/NavBar.module.css';
-import styles2 from '../cssModules/NavBar2.module.css';
 import { Link } from 'react-router-dom';
-import universe from '../Images/subImages/dodecahedron.png';
+import universe from '../myImages/subImages/dodecahedron.png';
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -11,7 +9,6 @@ class NavBar extends React.Component {
         this.listener = null;
         this.state = {
             status: "up",
-			styling: styles1,
         };
     }
 
@@ -40,18 +37,58 @@ class NavBar extends React.Component {
     render() {
         return (
             <div
-                className={this.state.styling.spacer}
+                className={this.props.theStyle.spacer}
                 style={{backgroundColor:
                         this.state.status === "up" ?
                         "transparent" : "white",
                         position: "fixed",
                         transition: "all 0.5s",
             }}>
-                <div className={this.state.styling.navbar}>
-                    <span className={this.state.styling.signature} ><img src={universe} id="Nav-logo"/></span>
-                    <Link to="/"><span className={this.state.styling.navbtn}>Home</span></Link>
-                    <Link to="/about"><span className={this.state.styling.navbtn}>About</span></Link>
-                    <Link to="/join"><span className={this.state.styling.navbtn}>Join</span></Link>
+                <div className={this.props.theStyle.navbar}>
+                    <span className={this.props.theStyle.signature} ><img src={universe} id="Nav-logo"/></span>
+                    <Link to="/"><span
+                                    style={{
+                                        color:
+                                        this.state.status === "up" ?
+                                        this.props.up : this.props.down
+                                    }}
+                                    className={
+                                        this.state.status === "up" ?
+                                        this.props.theStyle.navbtn1 :
+                                        this.props.theStyle.navbtn2
+
+                                    }
+                    >
+                        Home
+                    </span></Link>
+                    <Link to="/about"><span
+                                        style={{
+                                            color:
+                                            this.state.status === "up" ?
+                                            this.props.up : this.props.down
+                                        }}
+                                        className={
+                                            this.state.status === "up" ?
+                                            this.props.theStyle.navbtn1 :
+                                            this.props.theStyle.navbtn2
+                                        }
+                    >
+                        About
+                    </span></Link>
+                    <Link to="/join"><span
+                                        style={{
+                                            color:
+                                            this.state.status === "up" ?
+                                            this.props.up : this.props.down
+                                        }}
+                                        className={
+                                            this.state.status === "up" ?
+                                            this.props.theStyle.navbtn1 :
+                                            this.props.theStyle.navbtn2
+                                        }
+                    >
+                        Join
+                    </span></Link>
                 </div>
             </div>
         );
