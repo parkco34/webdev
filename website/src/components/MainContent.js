5962883d2eb7000ec15ed8f505bf7744a02ff3c5
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-31 19:38:28 -0400

    Update MainContent.js

--- website/src/components/MainContent.js
+++ website/src/components/MainContent.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
 import PaperSegment from './PaperSegment';
+import LearningSegment from './LearningSegment';
+import './MandroidBB.css';
 
 const useStyles = makeStyles(thingy => ({
 	toolbar: thingy.mixins.toolbar,
 	title: {
 		flexGrow: 1,
-		backgroundColor: thingy.palette.background.default,
 		padding: thingy.spacing(3),
         display: 'flex',
         justifyContent: 'center',
@@ -18,8 +20,8 @@ const useStyles = makeStyles(thingy => ({
 		padding: thingy.spacing(3),
         display: 'flex',
         justifyContent: 'center',
-        fontSize: '36px',
-        fontFamily: 'Lucida Console'
+        fontSize: '24px',
+        fontFamily: 'MandroidBB',
 	},
 	fullWidth: {
 		width: '100%',
@@ -36,23 +38,43 @@ function MainContent() {
 	const classes = useStyles();
 
 	return (
-		<main className={classes.fullWidth}>
-			<div className={classes.toolbar} />
-			<div className={classes.title}>
-				<Typography variant='h6' style={{fontFamily: 'youth-touch', fontSize: '48px', fontWeight: 'bold'}}>IT MUST BE IN THE WATER</Typography>
-			</div>
-			<div className={classes.content}>
-				<Typography className={classes.content}>
-					"The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently."
-				</Typography>
-			</div>
-			<div className={classes.content} style={flexStyle}>
-				<PaperSegment />
-			</div>
-		</main>
+        <div className="MainContent">
+            <section className={classes.fullWidth}>
+                <div className="spacer" />
+                <Divider style={{background: 'white'}}/>
+                <div className={classes.title}>
+                    <Typography variant='h6' style={{fontFamily: 'MandroidBB', fontSize: '48px', fontWeight: 'bold'}}>IT MUST BE IN THE WATER</Typography>
+                </div>
+                <div className={classes.content}>
+                    <Typography className={classes.content}>
+                        "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently."
+                    </Typography>
+                </div>
+                <div className={classes.content}>
+                    <PaperSegment />
+                </div>
+            </section>
+            <div className="spacer" style={{height: '50px'}}/>
+            <Divider style={{background: 'white'}}/>
+            <section className={classes.fullWidth}>
+                <div className={classes.title}>
+                   <Typography variant='h6' style={{fontFamily: 'MandroidBB', fontSize: '48px', fontWeight: 'bold'}}>
+                        Current Learning w/ Supplementation
+                    </Typography>
+                </div>
+                <div className={classes.content}>
+                    <Typography className={classes.content}>
+                        “Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.” – Richard Feynman
+                    </Typography>
+                </div>
+                <div className={classes.content}>
+                    <LearningSegment />
+                </div>
+            </section>
+            <div className="spacer" style={{height: '50px'}}/>
+            <Divider style={{background: 'white'}}/>
+        </div>
 	);
 }
 
 export default MainContent;
-
-
