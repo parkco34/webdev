5bc4878438a7c7d05b8b5e0e41114668d512373f
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-05-31 19:41:00 -0400

    Update Home.js

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import MainContent from '../comps/MainContent';
 import Background from '../comps/Background';
+import TheFooter from '../comps/footer/TheFooter';
+import NavBar from '../comps/NavBar.js';
+import styles1 from '../cssModules/NavBar.module.css';
 
 function Home() {
     return (
-		<div className="backgroundContainer">
+		<div style={{overflow: 'hidden'}}className="backgroundContainer">
+            <NavBar theStyle={styles1} up="white" down="black"/>
 			<Background />
 			<MainContent />
+            <TheFooter />
 		</div>
     );
 }
