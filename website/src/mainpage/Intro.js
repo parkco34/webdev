e7bb1df1cb80090cf9f29a052828c5a6f4b7767b
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-02 18:26:58 -0400

    Create Intro.js

---
+++ website/src/mainpage/Intro.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import TheBackground from './TheBackground';
+
+export default class Intro extends Component {
+    render() {
+        return (
+            <div className="Intro">
+                <TheBackground />
+            </div>
+        );
+    }
+}
