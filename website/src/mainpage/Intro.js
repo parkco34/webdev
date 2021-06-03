1d117b24aaf4a04d555bffabbd4fc7209dae7cec
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-03 16:00:59 -0400

    Update Intro.js

--- website/src/mainpage/Intro.js
+++ website/src/mainpage/Intro.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import TheBackground from './TheBackground';
+import Carousel, {CarouselItem} from './Carousel';
 
 export default class Intro extends Component {
     render() {
         return (
-            <div className="Intro">
-                <TheBackground />
-            </div>
+			<div className="Intro">
+				<div >
+					<Carousel >
+						<CarouselItem>Item1</CarouselItem>
+						<CarouselItem>Item2</CarouselItem>
+						<CarouselItem>Item3</CarouselItem>
+					</Carousel>
+				</div>
+			</div>
         );
     }
 }
