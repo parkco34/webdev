df991b356330c4800773bf3d504c40d6dc93ea3e
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-04 16:00:09 -0400

    Update Intro.js

--- website/src/mainpage/Intro.js
+++ website/src/mainpage/Intro.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
 import Carousel, {CarouselItem} from './Carousel';
+import image1 from '../Images/astronaut2.jpg';
+import image2 from '../Images/astronaut.jpg';
+import image3 from '../Images/monolith.jpg';
 
-export default class Intro extends Component {
+class Intro extends Component {
     render() {
         return (
 			<div className="Intro">
 				<div >
 					<Carousel >
-						<CarouselItem>Item1</CarouselItem>
-						<CarouselItem>Item2</CarouselItem>
-						<CarouselItem>Item3</CarouselItem>
+						<CarouselItem >
+							<img 
+								src={`${image1}`} 
+								alt="IMAGE NOT FOUND"
+								id="image1"
+								style={{width: '100%', height: '100%'}}
+							/>
+						</CarouselItem>
+						<CarouselItem >
+							<img 
+								src={`${image2}`} 
+								alt="IMAGE NOT FOUND"
+								style={{width: '100%', height: '100%'}}
+							/>
+						</CarouselItem>
+						<CarouselItem >
+							<img 
+								src={`${image3}`} 
+								alt="IMAGE NOT FOUND"
+								style={{width: '100%', height: '100%'}}
+							/>
+						</CarouselItem>
 					</Carousel>
 				</div>
 			</div>
         );
     }
 }
+
+export default Intro;
