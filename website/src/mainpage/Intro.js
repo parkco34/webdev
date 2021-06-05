08c8a5002c8d4482f1ecab95c9d727c3c7035273
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-05 16:47:11 -0400

    Update Intro.js

--- website/src/mainpage/Intro.js
+++ website/src/mainpage/Intro.js
@@ -3,14 +3,11 @@ import Carousel, {CarouselItem} from './Carousel';
 import image1 from '../myImages/astronaut2.jpg';
 import image2 from '../myImages/astronaut1.jpg';
 import image3 from '../myImages/monolith.jpg';
-import NavBar from '../comps/NavBar';
-import styles1 from '../cssModules/NavBar.module.css';
 
 class Intro extends Component {
     render() {
         return (
 			<div className="Intro">
-                <NavBar theStyle={styles1} up="white" down="black" />
 				<div >
 					<Carousel >
 						<CarouselItem >
