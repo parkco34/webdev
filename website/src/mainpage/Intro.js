e651830465c8ed74e7ab8d57af4f591ce5cb264b
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-04 21:27:30 -0400

    Update Intro.js

--- website/src/mainpage/Intro.js
+++ website/src/mainpage/Intro.js
@@ -1,33 +1,36 @@
 import React, {Component} from 'react';
 import Carousel, {CarouselItem} from './Carousel';
-import image1 from '../Images/astronaut2.jpg';
-import image2 from '../Images/astronaut.jpg';
-import image3 from '../Images/monolith.jpg';
+import image1 from '../myImages/astronaut2.jpg';
+import image2 from '../myImages/astronaut1.jpg';
+import image3 from '../myImages/monolith.jpg';
+import NavBar from '../comps/NavBar';
+import styles1 from '../cssModules/NavBar.module.css';
 
 class Intro extends Component {
     render() {
         return (
 			<div className="Intro">
+                <NavBar theStyle={styles1} up="white" down="black" />
 				<div >
 					<Carousel >
 						<CarouselItem >
-							<img 
-								src={`${image1}`} 
+							<img
+								src={`${image1}`}
 								alt="IMAGE NOT FOUND"
 								id="image1"
 								style={{width: '100%', height: '100%'}}
 							/>
 						</CarouselItem>
 						<CarouselItem >
-							<img 
-								src={`${image2}`} 
+							<img
+								src={`${image2}`}
 								alt="IMAGE NOT FOUND"
 								style={{width: '100%', height: '100%'}}
 							/>
 						</CarouselItem>
 						<CarouselItem >
-							<img 
-								src={`${image3}`} 
+							<img
+								src={`${image3}`}
 								alt="IMAGE NOT FOUND"
 								style={{width: '100%', height: '100%'}}
 							/>
@@ -40,3 +43,4 @@ class Intro extends Component {
 }
 
 export default Intro;
+
