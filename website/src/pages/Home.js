d3bb61bc021902afb062d80a12980cde35982ee3
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-06 15:09:55 -0400

    Update Home.js

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import MainContent from '../comps/MainContent';
+import MainBackground from '../comps/MainBackground';
+import Carousel from '../mainpage/Carousel2';
 import Background from '../comps/Background';
 import TheFooter from '../comps/footer/TheFooter';
 import NavBar from '../comps/NavBar.js';
 import styles1 from '../cssModules/NavBar.module.css';
+import theImage from '../myImages/astronaut2.jpg';
 
 function Home() {
     return (
 		<div style={{overflow: 'hidden'}}className="backgroundContainer">
-            		<NavBar theStyle={styles1} up="white" down="black"/>
-			<Background />
+            <NavBar theStyle={styles1} up="white" down="black"/>
+            <MainBackground image={theImage}/>
 			<MainContent />
-			<TheFooter />
+            <TheFooter />
 		</div>
     );
 }
 
 export default Home;
-
