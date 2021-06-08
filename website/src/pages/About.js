42c1b36f28280d111a10b8c554ec203869b27310
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-08 13:58:10 -0400

    Update About.js

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import NavBar from '../comps/NavBar';
+import NavBar from '../components/NavBar';
 import styles1 from '../cssModules/NavBar.module.css';
 import Carousel from '../mainpage/Carousel2.js';
+import ResponsiveNav from '../components/ResponsiveNav';
+
+//			<NavBar theStyle={styles1} up="white" down="black"/>
+//			<div className="main-container">
+//                <Carousel />
+//            </div>
 
 function About() {
     return (
-		<div >
-			<NavBar theStyle={styles1} up="white" down="black"/>
-			<div className="main-container">
-                <Carousel />
-            </div>
+		<div style={{backgroundColor: 'white', color: 'black', height: '100vh'}}>
+			<section className="sxn">
+				<ResponsiveNav />
+			</section>
 		</div>
 	);
 }
