e069f6a9dc96a192b3a179d670c4f89c7d59a270
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-08 20:50:00 -0400

    Update Goal.js

--- website/src/pages/Goal.js
+++ website/src/pages/Goal.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
 import theImage from '../myImages/data_book.jpg';
 import ResponsiveNav from '../comps/ResponsiveNav.js';
+import depression1 from "../myImages/depression1.webp";
+import alone from "../images/alone.webp";
+import pain from "../images/pain.webp";
 
 const Goal = props => {
     return (
         <div style={{backgroundColor: 'white', color: 'black', width: '100vw', height: '100vh'}}>
-            <ResponsiveNav />
+            <section className="sxn">
+                <ResponsiveNav />
+            </section>
+            <section>
+                <div className="sxn2-container">
+                    <img src={pain} alt="NO IMAGE FOUND "/>
+                    <img src={alone} alt="NO IMAGE FOUND "/>
+                    <img src={depression1} alt="NO IMAGE FOUND "/>
+                </div>
+            </section>
         </div>
     );
 }
