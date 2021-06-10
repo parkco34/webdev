ed21511a66481e4572350ba88e24c1bcf070c6b4
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-10 15:52:20 -0400

    Update RepsonsiveNav.js

--- website/src/components/RepsonsiveNav.js
+++ website/src/components/RepsonsiveNav.js
@@ -10,7 +10,7 @@ const Navigation = styled.header`
 	align-items: center;
 	padding: 0px 100px 0;
 	height: 100px;
-	margin-bottom: 60px;
+	margin-bottom: -73px;
     font-family: MandroidBB;
 
     em {
@@ -39,6 +39,7 @@ const Navigation = styled.header`
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
+			margin: 0 31%;
 		}
 		li {
 			margin: 0 45px;
@@ -57,7 +58,7 @@ const Navigation = styled.header`
 
 	.navbtn1 {
 		position: relative;
-		color: ${props => props.fontColor || "#222"};
+		color: ${props => props.fontColor || "#CBCCD1"};
 		opacity: 0.55;
 		transition: all 0.6s;
 		font-size: 1em;
@@ -69,7 +70,7 @@ const Navigation = styled.header`
 		right: 0;
 		width: 0;
 		bottom: -5px;
-		background: #222;
+		background: ${props => props.fontColor || "#CBCCD1"};
 		height: 3px;
 		transition-property: width;
 		transition-duration: 0.3s;
@@ -87,7 +88,7 @@ const Navigation = styled.header`
 
 	.navbtn2 {
 		position: relative;
-		color: black;
+		color: ${props => props.scrollingFontColor || "black"};
         list-style-type: none;
 	}
 
@@ -97,7 +98,7 @@ const Navigation = styled.header`
 		right: 0;
 		width: 0;
 		bottom: -5px;
-		background: black;
+		background: ${props => props.scrollingFontColor || "black"};
 		height: 3px;
 		transition-property: width;
 		transition-duration: 0.3s;
@@ -135,6 +136,21 @@ const Navigation = styled.header`
         right: 10px;
         cursor: pointer;
     }
+
+	.me-logo {
+		display: flex;
+		postition: relative;
+		position: fixed;
+		margin-top: -3.5%;
+		margin-left: 7%;
+		color:${props => props.fontColor || "black"};
+	}
+
+	.thelogo {
+		width: 85px;
+		height: 85px;
+		z-index: 1;
+	}
 `
 
 const ResponsiveNav = props => {
@@ -158,10 +174,17 @@ const ResponsiveNav = props => {
 	}
 
 	return (
-		<Navigation fontColor="white">
-			<div className="me-logo">
+		<Navigation fontColor={props.fontColor} scrollingFontColor={props.scrollingFontColor}>
+			<div className="logo-container">
 				<Link to="/">
-					<p style={{visibility: 'hidden'}}>For Lebert</p>
+					<p style={{visibility: 'hidden'}}>For Kyle Lebert</p>
+						<div className="me-logo" fontColor={props.fontColor}>
+							<img
+								src={props.logo} 
+								alt="No Logo found!" 
+								className="thelogo"
+							/>
+						</div>
 					<em>
 						<div className="secret-sauce">
 							<span className="hint" style={{color: props.hintColor, position: 'absolute', top: '73%', left: '73%'}}>Change the Title of the Webpage to: CLASSIFIED</span>
