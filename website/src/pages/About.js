cce05e2e71232e38ab391d642e3054e9a9083a0a
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-09 20:35:16 -0400

    Update About.js

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles1 from '../cssModules/NavBar.module.css';
 import Carousel from '../mainpage/Carousel2.js';
-import ResponsiveNav from '..comps/ResponsiveNav';
+import ResponsiveNav from '../comps/ResponsiveNav';
 
 function About() {
     return (
 		<div style={{backgroundColor: 'white', color: 'black', height: '100vh'}}>
-            <ResponsiveNav />
+            <ResponsiveNav hintColor="white"/>
 			<div className="main-container">
                 <Carousel />
             </div>
