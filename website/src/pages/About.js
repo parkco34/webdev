e6b2681530960055238bf1746778d5e4d982fe57
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-14 16:01:55 -0400

    Update About.js

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,5 +1,24 @@
+//import React from 'react';
+//import Carousel from '../mainpage/Carousel2.js';
+//import ResponsiveNav from '../components/ResponsiveNav';
+//import melogo2 from '../Images/logo22.jpg';
+//
+//function About() {
+//    return (
+//		<div style={{backgroundColor: 'white', color: 'black', height: '100vh'}}>
+//            <ResponsiveNav hintColor="white" fontcolor="black" logo={melogo2}/>
+//			<div className="main-container">
+//                <Carousel />
+//            </div>
+//		</div>
+//	);
+//}
+//
+//export default About;
+//
+
 import React from 'react';
-import Carousel from '../mainpage/Carousel2.js';
+import ExperimentationPage from './ExperimentationPage';
 import ResponsiveNav from '../comps/ResponsiveNav';
 import melogo2 from '../myImages/logo22.jpg';
 
@@ -8,13 +27,11 @@ function About() {
 		<div style={{backgroundColor: 'white', color: 'black', height: '100vh'}}>
             <ResponsiveNav hintColor="white" fontcolor="black" logo={melogo2}/>
 			<div className="main-container">
-                <Carousel />
+				<ExperimentationPage />
             </div>
 		</div>
 	);
 }
 
 export default About;
-
-
 
