993ef5b4b2fd200936217f3ba13c8163a4f70b38
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-15 20:28:09 -0400

    Update ExperimentalPage.js

--- website/src/pages/ExperimentalPage.js
+++ website/src/pages/ExperimentalPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ParallaxWrapper from '../comps/ParallaxWrapper';
-import image1 from '../myImages/subImages/misc1.jpg';
+import ParallaxWrapper from '../components/ParallaxWrapper';
+import image1 from '../Images/misc1.jpg';
 
 const styles = {
 	obj: {
