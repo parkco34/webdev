4a16d1ec153e372ee1f9c8ddf11548497e3618e8
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-18 15:40:51 -0400

    Create Programming.js

---
+++ website/src/components/learning/Programming.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import LearnReact from './LearnReact';
+
+const Programming = () => {
+	return (
+		<div>
+			<LearnReact />
+		</div>
+	);
+}
+
+export default Programming;
