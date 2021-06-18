4097d83f8f1d4befb9280d8bf82c07dc5029a88a
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-18 15:37:22 -0400

    Update Home.js

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -23,3 +23,29 @@ function Home() {
 }
 
 export default Home;
+
+//import React from 'react';
+//import ResponsiveNav from '../components/ResponsiveNav.js';
+//import MainContent from '../components/MainContent';
+//import MainBackground from '../components/MainBackground';
+//import Carousel from '../mainpage/Carousel2';
+//import Background from '../components/Background';
+//import TheFooter from '../components/footer/TheFooter';
+//import styles1 from '../cssModules/NavBar.module.css';
+//import theImage from '../Images/astronaut2.jpg';
+//import melogo1 from '../Images/logo2.jpg';
+//
+////<NavBar theStyle={styles1} up="white" down="black"/>
+//
+//function Home() {
+//    return (
+//		<div style={{overflow: 'hidden'}} className="backgroundContainer">
+//            <ResponsiveNav hintColor="black" fontcolor="white" scrollingFontColor="white" logo={melogo1}/>
+//            <MainBackground image={theImage}/>
+//			<MainContent />
+//            <TheFooter />
+//		</div>
+//    );
+//}
+//
+//export default Home;
