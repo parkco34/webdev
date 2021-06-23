cdaabf7525c4d3bf9a0998d77bf25baafda8259b
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-23 16:01:23 -0400

    Update About.js

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import Background from '../components/Background';
 import Carousel from '../mainpage/Carousel2.js';
-import ResponsiveNav from '../comps/ResponsiveNav';
-import melogo2 from '../myImages/logo22.jpg';
+import ResponsiveNav from '../components/ResponsiveNav';
+import melogo2 from '../Images/logo22.jpg';
 
 function About() {
+
     return (
 		<div >
             <ResponsiveNav hintColor="transparent" fontcolor="white" logo={melogo2}/>
 			<div className="main-container">
-                <Carousel />
+				<Background />
             </div>
 		</div>
 	);
 }
 
 export default About;
-
