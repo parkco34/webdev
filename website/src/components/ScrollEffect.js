b282a03b34ba99a051c8140a086549ca5e4c7992
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-06-23 16:04:49 -0400

    Update ScrollEffect.js

--- website/src/components/ScrollEffect.js
+++ website/src/components/ScrollEffect.js
@@ -1,16 +1,16 @@
 import React, {useState, useRef, useLayoutEffect} from 'react';
 import styled from 'styled-components';
-import image1 from '../myImages/reality_dilation.gif';
-import image2 from '../myImages/red_rainbow.jpg';
-import image3 from '../myImages/holyshit1.jpg';
-import defaultImage from '../myImages/bengal1.jpg';
+import image1 from '../Images/reality_dilation.gif';
+import image2 from '../Images/red_rainbow.jpg';
+import image3 from '../Images/holyshit1.jpg';
+import defaultImage from '../Images/bengal1.jpg';
 
 const Div = styled.div`
-	height: auto;
-	width: 600px;
-	background-image: url(${props => props.bkImage || defaultImage});
+	height: ${props => props.Height || "600px"};
+	width: ${props => props.Width || "600px"};
 	background-repeat: no-repeat;
-	transform: translateY(${({animate}) => (animate ? "0" : "-100vh")});
+	background-image: url(${props => props.bkImage || defaultImage});
+	transform: translateY(${({animate}) => (animate ? "-46vh" : "0")});
 	transition: transform 1s;
 	margin: 20px;
 	opacity: ${({animatePercent}) =>
@@ -48,7 +48,7 @@ const ScrollEffect = props => {
 		const div3Height = getHeight(lastRef.current);
 
 		const onScroll = () => {
-			const scrollPos = window.scrollY + window.innerheight;
+			const scrollPos = window.scrollY + window.innerHeight;
 
 			if (div1Pos < scrollPos) {
 				setShow(state => ({...state, itemOne: true}));
@@ -63,7 +63,7 @@ const ScrollEffect = props => {
 
 				let itemThreePercent = ((scrollPos - div3Pos) * 100) / div3Height;
 				if (itemThreePercent > 100) itemThreePercent = 100;
-				if (itemThreePercent > 0) itemThreePercent = 0;
+				if (itemThreePercent < 0) itemThreePercent = 0;
 
 				setPercentShow(prevState => ({
 					...prevState,
@@ -87,13 +87,27 @@ const ScrollEffect = props => {
 					animatePercent={percentShown.itemThree}
 					ref={lastRef}
 					bkImage={props.Img3}
+					Width={props.Width}
+					Height={props.Height}
 				>
 					<p>What</p>
 					<p>to put</p>
 					<p>right here?</p>
 				</Div>
-				<Div animate={show.itemTwo} ref={anotherRef} bkImage={props.Img2}/>
-				<Div animate={show.itemOne} ref={ourRef} bkImage={props.Img1}/>
+				<Div 
+					animate={show.itemTwo} 
+					ref={anotherRef} 
+					bkImage={props.Img2}
+					Width={props.Width}
+					Height={props.Height}
+				/>
+				<Div 
+					animate={show.itemOne} 
+					ref={ourRef} 
+					bkImage={props.Img1}
+					Width={props.Width}
+					Height={props.Height}
+				/>
 			</Wrapper>
 		</>
 	);
