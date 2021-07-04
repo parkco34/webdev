e64568a4d8a41a27636efb3f4ca7bcb0cb548753
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-04 17:34:12 -0400

    Update MainBackground.js

--- Church/src/components/MainBackground.js
+++ Church/src/components/MainBackground.js
@@ -11,7 +11,39 @@ const useStyles = makeStyles({
             display: 'flex',
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'contain',
-            backgroundPosition: 'center center',
+            backgroundPosition: 'center center',import React, {useState, useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        height: '95%',
+        width: '100vw',
+        margin: '-100px',
+
+        '& .main-image': {
+            display: 'flex',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            maxHeight: '100vh',
+            height: '100vh',
+        },
+    },
+});
+
+const MainBackground = props => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <div className="main-image" style={{backgroundImage: `url(${props.image})`}}>
+            </div>
+        </div>
+    );
+}
+
+export default MainBackground;
+
+
             maxHeight: '100vh',
             height: '100vh',
         },
