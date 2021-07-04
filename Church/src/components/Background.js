dfbd4025389ec8e3d1df7222d6d279678a927e96
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-03 21:23:06 -0400

    Update Background.js

--- Church/src/components/Background.js
+++ Church/src/components/Background.js
@@ -1,33 +1,67 @@
-import React, {useEffect, useState} from "react";
-import '../cssModules/Background.module.css';
-import mainImage from '../Images/cross_fractal.webp';
-import mobileImage from '../Images/devil.webp';
-import styles from '../cssModules/Background.module.css';
-import NavBar from './NavBar';
-
+import React, {useEffect, useState, useRef} from "react";
+import mainImage from '../Images/church2.jpg';
+import mainImage2 from '../Images/church3.jpg';
+import mobileImage from '../Images/church4.jpg';
+import styles from './Background.css';
 
 const Background = () => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [image, setImage] = useState(mainImage);
+	const [count, setCount] = useState(0);
+
     /* The empty array as a second argument prevents the useEffect hook from running every time the component updates (only when it mounts and unmounts is it run). <---????    */
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWindowWidth(window.innerWidth);
-        }
+	const useWindowWidth = () => {
+		const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-        window.addEventListener('resize', handleWindowResize);
+		useEffect(() => {
+			const handleWindowResize = () => {
+				setWindowWidth(window.innerWidth);
+			};
 
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        }
-    }, []);
+			window.addEventListener('resize', handleWindowResize);
+			return () => window.removeEventListener('resize', handleWindowResize);
+		}, []);
 
-    const image = window.innerWidth >= 650 ? mainImage : mobileImage;
+		return windowWidth;
+	};
+
+	function useInterval(callback, delay) {
+		const savedCallback = useRef();
+
+		useEffect(() => {
+			savedCallback.current = callback;
+		}, [callback]);
+
+		useEffect(() => {
+			function tick() {
+				savedCallback.current();
+			}
+
+			if (delay !== null) {
+				let id = setInterval(tick, delay);
+				return () => clearInterval(id);
+			}
+		}, [delay]);
+	}
+
+	useInterval(() => {
+		if ((image !== mainImage2) && (count % 7 === 0)) {
+			setImage(mainImage2);
+			setCount(count + 1);
+		}
+
+		if ((image !== mainImage) && (count % 7 === 0)) {
+			setImage(mainImage);
+			setCount(count + 1);
+		}
+		else {
+			setCount(count + 1);
+		}
+	}, 2000);
 
     return (
         <div className={styles.mainContainer}>
-            <div className={styles.theBackground} style={{backgroundImage: `url(${image})`}}>
+			<div className={styles.theBackground} style={{backgroundImage: `url(${image})`}}>
                 <div className={styles.backgroundContainer} >
-                    <NavBar />
                 </div>
             </div>
         </div>
@@ -35,3 +69,7 @@ const Background = () => {
 }
 
 export default Background;
+
+
+
+
