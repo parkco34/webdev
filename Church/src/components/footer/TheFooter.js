88c811cc1f488204b35915c23c054f7f5b6cae7c
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-03 21:37:40 -0400

    Create TheFooter.js

---
+++ Church/src/components/footer/TheFooter.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+		zIndex: '1',
+
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(51),
+            height: theme.spacing(59),
+        },
+    },
+	proper: props => ({
+		background: `url(${props.Background})`
+	})
+}));
+
+export default function ThePaper(props) {
+    const classes = useStyles(props);
+	const {proper} = props;
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={3} Background={props.Background} background={props.background}/>
+        </div>
+    );
+}
+
+
