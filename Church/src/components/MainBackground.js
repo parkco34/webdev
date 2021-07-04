506023622628f61bb458a80f84a1a7903dd8ddef
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-03 21:31:35 -0400

    Create MainBackground.js

---
+++ Church/src/components/MainBackground.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        height: '95%',
+        width: '100vw',
+        margin: '-100px',
+
+        '& .main-image': {
+            display: 'flex',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center center',
+            maxHeight: '100vh',
+            height: '100vh',
+        },
+    },
+});
+
+const MainBackground = props => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <div className="main-image" style={{backgroundImage: `url(${props.image})`}}>
+            </div>
+        </div>
+    );
+}
+
+export default MainBackground;
+
