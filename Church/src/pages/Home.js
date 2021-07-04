46b8332d2567c8dd14cfaccfb878602658b0c7ae
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-03 21:38:47 -0400

    Update Home.js

--- Church/src/pages/Home.js
+++ Church/src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Navbar from '../components/Navbar.js';
 import MainContent from '../components/MainContent';
+import MainBackground from '../components/MainBackground';
 import Background from '../components/Background';
+import TheFooter from '../components/footer/TheFooter';
+import sky from '../Images/sky2.jpg';
+import theImage from '../Images/church2.jpg';
 
 function Home() {
     return (
-		<div className="backgroundContainer">
-			<Background />
+		<div style={{overflow: 'hidden', backgroundImage: `url(${sky})`}} className="backgroundContainer">
+            <Navbar hintColor="transparent" fontcolor="black" scrollingFontColor="black"/>
+            <MainBackground image={theImage}/>
 			<MainContent />
+            <TheFooter />
 		</div>
     );
 }
 
 export default Home;
+
