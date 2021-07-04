32b767f74b0088b783b34338593d58e0167c9ccd
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-04 17:33:50 -0400

    Update Home.js

--- Church/src/pages/Home.js
+++ Church/src/pages/Home.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import Navbar from '../components/Navbar.js';
-import MainContent from '../components/MainContent';
-import MainBackground from '../components/MainBackground';
+import Main from '../components/Main';
 import Background from '../components/Background';
 import TheFooter from '../components/footer/TheFooter';
 import sky from '../Images/sky2.jpg';
-import theImage from '../Images/church2.jpg';
+import theImage from '../Images/last_supper.jpg';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        overflow: 'hidden',
+        backgroundImage: `url(${theImage})`,
+        backgroundPosition: '50% 75%',
+        height: '100vh',
+        display: 'flex',
+        position: 'relative',
+
+        '& .footer': {
+            display: 'flex',
+            position: 'absolute',
+            left: '0',
+            bottom: '0',
+            right: '0',
+            backgroundColor: 'black',
+        },
+    }
+});
 
 function Home() {
+    const classes = useStyles();
+
     return (
-		<div style={{overflow: 'hidden', backgroundImage: `url(${sky})`}} className="backgroundContainer">
-            <Navbar hintColor="transparent" fontcolor="black" scrollingFontColor="black"/>
-            <MainBackground image={theImage}/>
-			<MainContent />
-            <TheFooter />
+		<div className={classes.root}>
+            <div>
+                <Navbar hintColor="transparent" fontcolor="white" scrollingFontColor="white"/>
+                <Main />
+            </div>
+            <div className="footer">
+                <TheFooter />
+            </div>
 		</div>
     );
 }
