0aeb469f1c4bc417cb7fea32faae89344d386a31
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-05 19:47:38 -0400

    Update Home.js

--- Church/src/pages/Home.js
+++ Church/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Suspense, useState, useEffect} from 'react';
 import Navbar from '../components/Navbar.js';
-import Main from '../components/Main';
 import Background from '../components/Background';
 import TheFooter from '../components/footer/TheFooter';
 import sky from '../Images/sky2.jpg';
 import theImage from '../Images/last_supper.jpg';
+import logo from '../Images/ucc-logo.png';
 import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -24,17 +24,30 @@ const useStyles = makeStyles({
             right: '0',
             backgroundColor: 'black',
         },
+        '.fade-to-user': {
+            animation: 'fade 0.9s ease-in',
+        },
+        '@keyframes fade': {
+            '0%': {
+                opacity: '0',
+            }
+        }
     }
 });
 
+const Main = React.lazy(() => import('../components/Main'));
+
 function Home() {
     const classes = useStyles();
+    const [state, setState] = useState('true');
 
     return (
 		<div className={classes.root}>
             <div>
-                <Navbar hintColor="transparent" fontcolor="white" scrollingFontColor="white"/>
-                <Main />
+                <Navbar hintColor="transparent" fontcolor="white" scrollingFontColor="white" logo={logo}/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Main />
+                </Suspense>
             </div>
             <div className="footer">
                 <TheFooter />
