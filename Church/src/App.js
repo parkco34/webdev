dbfc62b73f43387cffba67adeb71a9f362b46a27
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-06 11:27:48 -0400

    Update App.js

--- Church/src/App.js
+++ Church/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Join from './pages/Join';
-import NoMatch from './pages/NoMatch';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Join from '../pages/Join';
 
 function App() {
     return (
-        <div>
+        <div >
             <Router>
                 <Switch>
                     <Route exact path="/" component={ Home } />
                     <Route exact path="/about" component={ About } />
                     <Route exact path="/join" component={ Join } />
-                    <Route component={ NoMatch } />
                 </Switch>
             </Router>
         </div>
@@ -21,3 +19,4 @@ function App() {
 }
 
 export default App;
+
