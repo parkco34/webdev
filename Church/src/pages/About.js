7ba5dd968a8a5f7ab3886bdbdd4594447eeea84c
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-09 15:49:28 -0400

    Update About.js

--- Church/src/pages/About.js
+++ Church/src/pages/About.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Navbar from '../components/Navbar';
+import logo from '../Images/ucc-logo.png';
 
 function About() {
+	const [win, setWin] = useState(window.innerWidth);
+
+	useEffect(() => {	
+		function handleWindow() {
+			setWin(window.innerWidth);
+		}
+		
+		window.addEventListener('resize', handleWindow);
+		return () => window.removeEventListener('resize', handleWindow);
+	}, []);
+
     return (
-        <p>Dis be the fuckin About page!</p>
+		<div className="About">
+			<Navbar hintColor="transparent" fontColor="black" scrollingFontColor="black" logo={logo} />
+			<div className="something" style={{textAlign: 'center', width: '400px', margin: '300px 0 0 800px ', fontWeight: 'bold', fontSize: '34px', fontFamily: 'Lucida Console', color: 'limegreen', backgroundColor: 'black'}} >
+				<p className="paragraph">{`Window size = ${win}`}</p>
+			</div>
+		</div>
     );
 }
 
-export default About;
+export default About
