dbafc2ede77caa85a0de3f9f740d7269d79d1504
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-09 15:49:09 -0400

    Update Home.js

--- Church/src/pages/Home.js
+++ Church/src/pages/Home.js
@@ -1,10 +1,9 @@
 import React, {Suspense, useState, useEffect} from 'react';
-import Navbar from '../components/Navbar.js';
 import Background from '../components/Background';
 import TheFooter from '../components/footer/TheFooter';
 import Main from '../components/Main';
 import sky from '../Images/sky2.jpg';
-import theImage from '../Images/last_supper.jpg';
+import theImage from '../Images/last_supper0.JPG';
 import logo from '../Images/ucc-logo.png';
 import {makeStyles} from '@material-ui/core/styles';
 import FadeInAnimation from '../components/FadeIn';
@@ -17,34 +16,47 @@ const useStyles = makeStyles({
         height: '100vh',
 		width: '100vw',
         display: 'flex',
-        position: 'relative',
 
-        '& .footer': {
-            display: 'flex',
-            position: 'absolute',
-            left: '0',
-            bottom: '0',
-            right: '0',
-            backgroundColor: 'black',
-        },
-        '.fade-to-user': {
-            animation: 'fade 0.9s ease-in',
-        },
+		'& .home-fade': {
+			height: '0',
+			width: '0',
+		},
+		'& .odd-div': {
+				position: 'absolute',
+				margin: '300px 0 500px 600px',
+				height: '200px',
+				width: '500px',
+				color: 'white',
+				fontFamily: 'Lucida Console',
+				fontWeight: 'bold',
+		}
     }
 });
 
 function Home() {
     const classes = useStyles();
+	const [windowDimension, setWindowDimension] = useState(window.innerWidth);
+
+	useEffect(() => {
+		function handleWindow() {
+			setWindowDimension(window.innerWidth);
+		}
+
+		window.addEventListener('resize', handleWindow);
+		return () => window.removeEventListener('resize', handleWindow);
+	}, [])
 
     return (
 		<div className={classes.root}>
-			<FadeInAnimation direction="down">
-                <Navbar hintColor="transparent" fontcolor="white" scrollingFontColor="white" logo={logo}/>
+			<FadeInAnimation direction="down" className="home-fade tp">
 			</FadeInAnimation>
-			<FadeInAnimation>
-				<Main />
+			<FadeInAnimation >
+				<Main className="the-main"/>
 			</FadeInAnimation>
-			<FadeInAnimation direction="up" className="footer">
+			<div className="odd-div">
+				<p className="texty">{`Window Width = ${windowDimension}`}</p>
+			</div>
+			<FadeInAnimation direction="up" className="home-footer" >
 				<TheFooter />
 			</FadeInAnimation>
 		</div>
@@ -52,4 +64,5 @@ function Home() {
 }
 
 export default Home;
+
 
