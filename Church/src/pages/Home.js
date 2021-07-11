09dce926290374d021ed32785449ae5565450c8e
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-11 19:18:25 -0400

    Update Home.js

--- Church/src/pages/Home.js
+++ Church/src/pages/Home.js
@@ -1,68 +1,40 @@
-import React, {Suspense, useState, useEffect} from 'react';
-import Background from '../components/Background';
-import TheFooter from '../components/footer/TheFooter';
-import Main from '../components/Main';
-import sky from '../Images/sky2.jpg';
-import theImage from '../Images/last_supper0.JPG';
-import logo from '../Images/ucc-logo.png';
-import {makeStyles} from '@material-ui/core/styles';
-import FadeInAnimation from '../components/FadeIn';
+import React from 'react';
+import Test from '../components/Test';
+import styled from 'styled-components';
+import TheFooter from '../footer/TheFooter';
+import theImage from '../Images/last_supper.jpg';
 
-const useStyles = makeStyles({
-    root: {
-        overflow: 'hidden',
-        backgroundImage: `url(${theImage})`,
-        backgroundPosition: '50% 75%',
-        height: '100vh',
-		width: '100vw',
-        display: 'flex',
-
-		'& .home-fade': {
-			height: '0',
-			width: '0',
-		},
-		'& .odd-div': {
-				position: 'absolute',
-				margin: '300px 0 500px 600px',
-				height: '200px',
-				width: '500px',
-				color: 'white',
-				fontFamily: 'Lucida Console',
-				fontWeight: 'bold',
-		}
-    }
-});
+const Main = {
+    Wrapper: styled.main`
+        display: flex;
+        justify-content: center;
+        background: ${props => `url(${props.backgroundImage})`};
+        background-size: cover;
+        background-position: 50% 75%;
+        background-repeat: no-repeat;
+        max-width: 100vw;
+        max-height: 100vh;
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        background-color: transparent;
+        z-index: -1;
+        overflow: hidden;
+    `,
+}
 
 function Home() {
-    const classes = useStyles();
-	const [windowDimension, setWindowDimension] = useState(window.innerWidth);
-
-	useEffect(() => {
-		function handleWindow() {
-			setWindowDimension(window.innerWidth);
-		}
-
-		window.addEventListener('resize', handleWindow);
-		return () => window.removeEventListener('resize', handleWindow);
-	}, [])
-
     return (
-		<div className={classes.root}>
-			<FadeInAnimation direction="down" className="home-fade tp">
-			</FadeInAnimation>
-			<FadeInAnimation >
-				<Main className="the-main"/>
-			</FadeInAnimation>
-			<div className="odd-div">
-				<p className="texty">{`Window Width = ${windowDimension}`}</p>
-			</div>
-			<FadeInAnimation direction="up" className="home-footer" >
-				<TheFooter />
-			</FadeInAnimation>
-		</div>
+        <div className="Main" >
+            <Main.Wrapper backgroundImage={theImage}>
+                <Test />
+            </Main.Wrapper>
+            <TheFooter />
+        </div>
     );
 }
 
 export default Home;
-
-
