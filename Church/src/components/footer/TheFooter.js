72dbc5ac9d7ffcb7f030b16c9cb89c1518d34466
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-11 19:20:32 -0400

    Update TheFooter.js

--- Church/src/components/footer/TheFooter.js
+++ Church/src/components/footer/TheFooter.js
@@ -1,26 +1,14 @@
 import React, {Component} from 'react';
 import SocialFollow from './SocialFollow';
-import styled from 'styled-components';
-
-const Foot = {
-	Loose: styled.div`
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100vw;
-		background-color: black;
-		z-index: 2;
-	`,
-};
 
 export default class TheFooter extends Component {
     render() {
         return (
-            <Foot.Loose>
-               <div>
+            <div className="Footer">
+               <div className="foot-container" style={{position: 'relative'}}>
                     <SocialFollow />
                 </div>
-            </Foot.Loose>
+            </div>
         );
     }
 }
