86eb7208ef3ff51bf860eb02b441db10ea57df7e
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-12 13:30:56 -0400

    Update Footer.js

--- Church/src/footer/Footer.js
+++ Church/src/footer/Footer.js
@@ -1,1 +1,41 @@
-i
+import React from 'react';
+import {
+	Box,
+	Column,
+	Row,
+	Container,
+	FooterLink,
+} from '../styles/FooterStyles';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+	return (
+		<Box>
+			<h1 style={{
+				color: 'white',
+				textAlign: 'center',
+				margintop: '-50px'
+				}}>
+			</h1>
+			<Container>
+				<Row>
+					<Column>
+						<FooterLink href="https://www.facebook.com/LakevilleUCC/"><FontAwesomeIcon color="blue" icon={faFacebook} size="2x" /></FooterLink>
+					</Column>
+
+					<Column classname="col-txt">
+						<FooterLink className="col-txt-child"><span className="label-txt">Hahn Auto Aftermarket Parts &copy; 2021</span></FooterLink>
+					</Column>
+				</Row>
+			</Container>
+		</Box>
+	);
+}
+
+export default Footer;
