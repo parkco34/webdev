26b4246414b194a73cda63792dea3a3509e1c019
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-12 11:04:46 -0400

    Update TheFooter.js

--- Church/src/components/footer/TheFooter.js
+++ Church/src/components/footer/TheFooter.js
@@ -1,15 +1,41 @@
-import React, {Component} from 'react';
-import SocialFollow from './SocialFollow';
+import React from 'react';
+import {
+	Box,
+	Column,
+	Row,
+	Container,
+	FooterLink,
+} from '../styles/FooterStyles';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
-export default class TheFooter extends Component {
-    render() {
-        return (
-            <div className="Footer">
-               <div className="foot-container" style={{position: 'relative'}}>
-                    <SocialFollow />
-                </div>
-            </div>
-        );
-    }
+const Footer = () => {
+	return (
+		<Box>
+			<h1 style={{
+				color: 'white',
+				textAlign: 'center',
+				margintop: '-50px'
+				}}>
+			</h1>
+			<Container>
+				<Row>
+					<Column>
+						<FooterLink href="https://www.facebook.com/LakevilleUCC/"><FontAwesomeIcon color="blue" icon={faFacebook} size="2x" /></FooterLink>
+					</Column>
+
+					<Column classname="col-txt">
+						<FooterLink className="col-txt-child"><span className="label-txt">Hahn Auto Aftermarket Parts &copy; 2021</span></FooterLink>
+					</Column>
+				</Row>
+			</Container>
+		</Box>
+	);
 }
 
+export default Footer;
