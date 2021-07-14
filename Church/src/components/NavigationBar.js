b5ff14eb79923cfa425239df677e0bea52cb3abf
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-14 15:25:21 -0400

    Update NavigationBar.js

--- Church/src/components/NavigationBar.js
+++ Church/src/components/NavigationBar.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import logo from '../Images/ucc-logo.png'
+import {Home, User, Info} from 'react-feather';
+import {NavLink, Link} from 'react-router-dom';
 
-const Styles = styled.main`
+const Styles = styled.div`
     display: flex;
-    background-color: #eeeeee;
-    height: 100vh;
+    background-color: transparent;
+	font-size: 16px;
 `;
 
 const Navbar = {
@@ -12,20 +15,62 @@ const Navbar = {
         display: flex;
         flex: 1;
         align-self: flex-start;
-        padding: 1rem 3rem;
+        padding: 1rem 7rem;
         justify-content: space-between;
         align-items: center;
+		color white;
+		z-index: 1;
+
+		a {
+			text-decoration: none;
+		}
+
+		a.active {
+			color: grey;
+		}
+
+		.navbtn1 {
+			position: relative;
+			list-style-type: none;
+			color: ${props => props.fontColor || "white"};
+			opacity: 1;
+			transition: all 0.6s;
+		}
+		.navbtn1:after {
+			content: "";
+			position: absolute;
+			right: 0;
+			width: 0;
+			bottom: -5px;
+			height: 3px;
+			transition-proerty: width;
+			transition-duration: 0.3s;
+			transition-timing-function: ease-out;
+			background: ${props => props.fontColor || "white"};
+		}
+
+		.navbtn1:hover:after,
+		.navbtn1:focus:after,
+		.navbt1:active:after {
+			left: 0;
+			right: auto;
+			opacity: 1;
+			width: 100%;
+		}
     `,
-    Logo: styled.div`
+    Logo: styled.img`
         border: 1px solid grey;
-        padding: 0.5rem 1rem;
+		margin: 0 0 0 4%;
+		width: 5.5rem;
+		height: 5.5rem;
     `,
     Items: styled.ul`
         display: flex;
         list-style: none;
+		margin-right: 4rem;
     `,
     Item: styled.li`
-        padding: 0 1rem;
+        padding: 0 2rem 0 4rem;
         cursor: pointer;
     `,
 };
@@ -37,9 +82,12 @@ const MobileNav = {
         width: 100vw;
         bottom: 0;
         justify-content: center;
+
+		a {
+			text-decoration: none;
+		}
     `,
     Logo: styled(Navbar.Logo)`
-
     `,
     Items: styled(Navbar.Items)`
         flex: 1;
@@ -47,41 +95,78 @@ const MobileNav = {
         justify-content: space-around;
     `,
     Item: styled(Navbar.Item)`
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 1.2rem;
     `,
+	Icon: styled.span``,
 };
 
-const isMobile = windowDimension <= 640;
-
-const NavigationBar = props => {
-    const [windowDimension, setWindowDimension] = useState(null);
+const NavigationBar = props => {	
+    const [windowDimension, setWindowDimension] = useState(window.innerWidth);
 
     useEffect(() => {
         function handleWindow() {
-            setWindowDimnesion(window.innerWidth);
+            setWindowDimension(window.innerWidth);
         }
 
         window.addEventListener('resize', handleWindow);
-        return () => window.removeEventListeneR('resize', handleWindow);
+        return () => window.removeEventListener('resize', handleWindow);
     }, []);
+
+	const isMobile = windowDimension <= 640;
 
     return (
         <div className="NavigationBar">
             <Styles>
                 {isMobile ? (
-                    <MobiNav.Wrapper>
+                    <MobileNav.Wrapper>
                         <MobileNav.Items>
-                            <MobileNav.Item>Home</MobileNav.Item>
-                            <MobileNav.Item>About</MobileNav.Item>
-                            <MobileNav.Item>Join</MobileNav.Item>
+							<NavLink activeClassName="active" to="/">
+								<MobileNav.Item className="navbtn1">
+									<MobileNav.Icon>
+										<Home size={16} />
+									</MobileNav.Icon>
+									Home
+								</MobileNav.Item>
+							</NavLink>
+							
+							<NavLink activeClassName="active" to="/about">
+								<MobileNav.Item className="navbtn1">
+									<MobileNav.Icon>
+										<Info size={16} />
+									</MobileNav.Icon>
+									About
+								</MobileNav.Item>
+							</NavLink>
+							
+							<NavLink activeClassName="active" to="/join">
+								<MobileNav.Item className="navbtn1">
+									<MobileNav.Icon>
+										<User size={16} />
+									</MobileNav.Icon>
+									Join
+								</MobileNav.Item>
+							</NavLink>
+
                         </MobileNav.Items>
-                    </MobiNav.Wrapper>
+                    </MobileNav.Wrapper>
                     ) : (
                     <Navbar.Wrapper>
-                        <Navbar.Logo></Navbar.Logo>
+						<Navbar.Logo src={logo} alt="NO LOGO FOUND!"/> 
                         <Navbar.Items>
-                            <Navbar.Item>Home</Navbar.Item>
-                            <Navbar.Item>About</Navbar.Item>
-                            <Navbar.Item>Join</Navbar.Item>
+							<NavLink activeClassName="active" to="/">
+								<Navbar.Item className="navbtn1">Home</Navbar.Item>
+							</NavLink>
+							
+							<NavLink activeClassName="active" to="/about">
+								<Navbar.Item className="navbtn1">About</Navbar.Item>
+							</NavLink>
+
+							<NavLink acitveClassName="active" to="/join">
+								<Navbar.Item className="navbtn1">Join</Navbar.Item>
+							</NavLink>
                         </Navbar.Items>
                     </Navbar.Wrapper>
                     )
@@ -90,4 +175,6 @@ const NavigationBar = props => {
         </div>
     );
 }
+
+export default NavigationBar;
 
