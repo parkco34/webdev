80fe0d6b0b9f473f9d8cf05a68a07a6dd125e8c6
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-14 18:49:59 -0400

    Update App.js

--- Church/src/components/App.js
+++ Church/src/components/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             <Router>
-                <NavigationBar />
+                <NavigationBar Color="black"/>
                 <Switch>
                     <Route path="/" exact component={() => <Home />} />
                     <Route path="/about" exact component={() => <About />} />
@@ -20,4 +20,5 @@ function App() {
 }
 
 export default App;
+
 
