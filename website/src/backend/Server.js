d306bbd4c17e1303d8a1368777257503b8ca84f9
Cory Parker <44587752+parkco34@users.noreply.github.com>
2021-07-18 20:11:53 -0400

    Update Server.js

--- website/src/backend/Server.js
+++ website/src/backend/Server.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require(''mongoose);
-require('dotenv').config();
-const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: "http://localhost:3002"
+};
 
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const db = require('./models');
+db.mongoose.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then(() => {
+        console.log("Connected to database, YO!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the fucking database... FUUUUUCK!!!!", err);
+        process.exit();
+    });
+
+app.get('/', (req, res) => {
+    res.json({message: "Hello, Friend...\nThis is my message to me!"});
+});
+
+require('./routes/login.routes')(app);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
